34f6a6208e6e5bc51680aaaa9c32df20a7dd18b1
tusharjain1082 <43837276+tusharjain1082@users.noreply.github.com>
2021-12-01 14:00:24 +0530

    Add files via upload

--- filedisk-proxy-test/prepare-start-new-session.cmd
+++ filedisk-proxy-test/prepare-start-new-session.cmd
@@ -2,4 +2,4 @@ sc stop filedisk-proxy
 sc delete filedisk-proxy
 sc create filedisk-proxy binpath="%CD%\filedisk-proxy.sys" type=kernel
 sc start filedisk-proxy
-"%CD%\proxyapp.exe"
+"%CD%\FilediskProxyNet.exe"
