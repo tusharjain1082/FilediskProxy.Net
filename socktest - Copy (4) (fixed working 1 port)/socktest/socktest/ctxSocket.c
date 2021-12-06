73cc813cf7d84b585d27145a4d203ab75f205dbc
tusharjain1082 <43837276+tusharjain1082@users.noreply.github.com>
2021-12-06 18:01:43 +0530

    Add files via upload

---
+++ socktest - Copy (4) (fixed working 1 port)/socktest/socktest/ctxSocket.c
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include <ntifs.h>
+#include <windef.h>
+#include <stdlib.h> 
+#include <ntstrsafe.h>
+#include <stdio.h>
+#include <string.h>
+#include <wsk.h>
+#include "shared.h"
+
+#include "ksocket.h"
+#include "berkeley.h"
+
+
+
