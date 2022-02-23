32fb9e0cdfe946fdefb64f40497126058a2b1dd1
tusharjain1082 <43837276+tusharjain1082@users.noreply.github.com>
2022-02-23 18:08:24 +0530

    Add files via upload

--- FilediskProxyNet/FilediskProxyNet/FilediskProxyNet/Program.cs
+++ FilediskProxyNet/FilediskProxyNet/FilediskProxyNet/Program.cs
@@ -16,7 +16,7 @@ namespace FilediskProxyNet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormRoot());
+            Application.Run(new FormFDProxy());
         }
     }
 }
