3a9074160aa7e5e9f868101e10296338d599d3ea
tusharjain1082 <43837276+tusharjain1082@users.noreply.github.com>
2022-02-24 12:02:25 +0530

    Delete FilediskProxyNet directory

--- FilediskProxyNet/FilediskProxyNet/filedisk-proxy/SHM/helper.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <ntifs.h>
-
-NTSTATUS CreateStandardSCAndACL(OUT PSECURITY_DESCRIPTOR* SecurityDescriptor, OUT PACL* Acl);
-NTSTATUS GrantAccess(HANDLE hSection, IN PACL StandardAcl);
